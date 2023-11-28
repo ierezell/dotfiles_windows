1175eaff7cb33f23c666c52d5f48290d1e8c22fc
Pierre Snell <psnell@biotwin.ai>
2023-11-28 12:17:02 -0500

    Add .config/nvim/LICENSE Add .config/nvim/init.lua Add .config/nvim/lazy-lock.json Add .config/nvim/lua/core/bootstrap.lua Add .config/nvim/lua/core/default_config.lua Add .config/nvim/lua/core/init.lua Add .config/nvim/lua/core/mappings.lua Add .config/nvim/lua/core/utils.lua Update .config/nvim/lua/custom/plugins.lua Add .config/nvim/lua/plugins/configs/cmp.lua Add .config/nvim/lua/plugins/configs/lazy_nvim.lua Add .config/nvim/lua/plugins/configs/lspconfig.lua Add .config/nvim/lua/plugins/configs/mason.lua Add .config/nvim/lua/plugins/configs/nvimtree.lua Add .config/nvim/lua/plugins/configs/others.lua Add .config/nvim/lua/plugins/configs/telescope.lua Add .config/nvim/lua/plugins/configs/treesitter.lua Add .config/nvim/lua/plugins/init.lua

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -98,9 +98,6 @@ local plugins = {
         end,
       },
     },
-    config = function()
-      require "custom.configs.dap"
-    end,
   },
 
   {
@@ -135,46 +132,6 @@ local plugins = {
     dependencies = "neovim/nvim-lspconfig",
     init = function()
       vim.g.rustfmt_autosave = 1
-    end,
-    opts = function()
-      local ok, mason_registry = pcall(require, "mason-registry")
-      local adapter ---@type any
-      if ok then
-        -- rust tools configuration for debugging support
-        local codelldb = mason_registry.get_package "codelldb"
-        local extension_path = codelldb:get_install_path() .. "/extension/"
-        local codelldb_path = extension_path .. "adapter/codelldb"
-        local liblldb_path = ""
-        if vim.loop.os_uname().sysname:find "Windows" then
-          liblldb_path = extension_path .. "lldb\\bin\\liblldb.dll"
-        elseif vim.fn.has "mac" == 1 then
-          liblldb_path = extension_path .. "lldb/lib/liblldb.dylib"
-        else
-          liblldb_path = extension_path .. "lldb/lib/liblldb.so"
-        end
-        adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-      end
-      return {
-        dap = { adapter = adapter },
-        tools = {
-          runnables = { use_telescope = true },
-          inlay_hints = {
-            auto = true,
-            show_parameter_hints = false,
-            parameter_hints_prefix = "",
-            other_hints_prefix = "",
-          },
-          on_initialized = function()
-            vim.cmd [[
-              augroup RustLSP
-                autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
-                autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
-                autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
-              augroup END
-            ]]
-          end,
-        },
-      }
     end,
   },
 
