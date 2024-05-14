c898ed81b2a495c10d0e1ecfa140f5e48d655821
ierezell <ierezell@gmail.com>
2024-05-14 14:54:08 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -59,6 +59,20 @@ require('lazy').setup(
       end
     },
 
+    -- Rest client
+    {
+      "vhyrro/luarocks.nvim",
+      priority = 1000,
+      config = true,
+      opts = { rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" } }
+    },
+    {
+      "rest-nvim/rest.nvim",
+      ft = "http",
+      dependencies = { "luarocks.nvim" },
+      config = function() require("rest-nvim").setup() end,
+    },
+
 
     -- Float or stack terminal
     {
