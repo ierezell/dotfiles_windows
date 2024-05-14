5002a03d52caba8fcdfccdd29ea0e1998e05512d
ierezell <ierezell@gmail.com>
2024-05-14 10:42:57 -0400

    Update .config/i3/config Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -33,7 +33,27 @@ require('lazy').setup(
       'goolord/alpha-nvim', -- Start-up menu
       dependencies = { 'nvim-tree/nvim-web-devicons' },
       config = function()
-        require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+        local alpha = require 'alpha'
+        local dashboard = require 'alpha.themes.dashboard'
+        dashboard.section.header.val = {
+          [[                                   __                ]],
+          [[      ___     ___    ___   __  __ /\_\    ___ ___    ]],
+          [[     / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+          [[    /\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+          [[    \ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+          [[     \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+        }
+
+        dashboard.section.buttons.val = {
+          dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+          dashboard.button("f", "  > Find file", ":cd /mnt/data | Telescope find_files<CR>"),
+          dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+          dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+          dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+        }
+
+        alpha.setup(dashboard.config)
+        vim.cmd([[ autocmd FileType alpha setlocal nofoldenable ]])
       end
     },
     {
