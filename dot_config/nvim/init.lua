12eed3a34fc351f46e602ce7ca3139e57ee4d8f2
ierezell <ierezell@gmail.com>
2024-05-14 14:18:51 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -60,7 +60,20 @@ require('lazy').setup(
     },
     {
       -- Float or stack terminal
-      { 'akinsho/toggleterm.nvim', version = "*", config = true }
+      {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        opts = {
+          size = 20 | function(term)
+            if term.direction == "horizontal" then
+              return 15
+            elseif term.direction == "vertical" then
+              return vim.o.columns * 0.4
+            end
+          end,
+          open_mapping = [[<c-`>]]
+        }
+      }
     },
     {
       'navarasu/onedark.nvim', -- Theme inspired by Atom
