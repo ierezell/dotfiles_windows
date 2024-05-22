8260bcbe47cc0338f029905ddc41d62c6eb1415f
ierezell <ierezell@gmail.com>
2024-05-22 15:32:00 -0400

    Update .config/nvim/lazy-lock.json Add .config/nvim/lua/plugins/code/codeium.lua Update .config/nvim/lua/settings/code/completions.lua

--- dot_config/nvim/lua/settings/code/completions.lua
+++ dot_config/nvim/lua/settings/code/completions.lua
@@ -1,22 +1,18 @@
 -- See `:help cmp`
 local cmp = require('cmp')
-local luasnip = require('luasnip')
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
 
 cmp.setup(
     {
-        sources = {{name = 'nvim_lsp'}, {name = 'path'}}, --
+        sources = {
+            { name = 'codeium' },
+            { name = 'nvim_lsp' },
+            { name = 'path' }
+        }, --
         mapping = cmp.mapping.preset.insert(
             {
-                ['<CR>'] = cmp.mapping.confirm({select = false}), -- Enter key confirms completion item
-                ['<C-Space>'] = cmp.mapping.complete() -- Ctrl + space triggers completion menu
-            } --
-        ), --
-        snippet = {
-            expand = function(args)
-                require('luasnip').lsp_expand(args.body)
-            end --
-        } --
-    } --
-) --
+                ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Enter key confirms completion item
+                ['<C-Space>'] = cmp.mapping.complete()              -- Ctrl + space triggers completion menu
+            }                                                       --
+        ),                                                          --
+    }
+)
