c57fcc7dfd99647cd02964bfa7a5b0b65838bc92
ierezell <ierezell@gmail.com>
2024-06-03 10:38:57 -0400

    Update .config/nvim/lua/settings/vimconfig.lua

--- dot_config/nvim/lua/settings/vimconfig.lua
+++ dot_config/nvim/lua/settings/vimconfig.lua
@@ -7,6 +7,7 @@ vim.o.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
+vim.wo.relativenumber = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -14,8 +15,8 @@ vim.o.mouse = 'a'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard='unnamedplus'
-vim.o.clipboard='unnamedplus'
+vim.opt.clipboard = 'unnamedplus'
+vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -41,14 +42,21 @@ vim.o.completeopt = 'menuone,noselect'
 vim.o.termguicolors = true
 
 vim.o.scrolloff = 15
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with( vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false, signs=false })
-vim.diagnostic.config({virtual_text = false, signs=false})
+
+-- Set terminal tab title to `filename (cwd)`
+vim.opt.title = true
+vim.opt.titlestring = '%t%( (%{fnamemodify(getcwd(), ":~:.")})%)'
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
+    { virtual_text = false, signs = false })
+
+vim.diagnostic.config({ virtual_text = false, signs = false })
 
 
 -- Remap <Esc> to <C-\><C-n> in terminal mode (for toggleterm)
 vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 
 -- Map Ctrl-S to save the current buffer
-vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>',{ noremap = true, silent = true })
-vim.api.nvim_set_keymap('i', '<C-s>', '<C-o>:w<CR>',{ noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<C-s>', '<C-o>:w<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('v', '<C-s>', '<C-c>:w<CR>', { noremap = true, silent = true })
