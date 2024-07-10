192683a203adc53bca380ee0a75380b43c1521cc
Pierre Snell <psnell@biotwin.ai>
2024-07-10 13:34:38 -0400

    Add .config/nvim/LICENSE Add .config/nvim/.gitignore Add .config/nvim/.ignore Add .config/nvim/.stylua.toml Add .config/nvim/lua/core/bootstrap.lua Add .config/nvim/lua/core/default_config.lua Add .config/nvim/lua/core/init.lua Add .config/nvim/lua/core/mappings.lua Add .config/nvim/lua/core/utils.lua Add .config/nvim/lua/custom/README.md Add .config/nvim/lua/custom/chadrc.lua Add .config/nvim/lua/custom/configs/lspconfig.lua Add .config/nvim/lua/custom/configs/none-ls.lua Add .config/nvim/lua/custom/configs/null-ls.lua Add .config/nvim/lua/custom/configs/overrides.lua Add .config/nvim/lua/custom/highlights.lua Add .config/nvim/lua/custom/init.lua Add .config/nvim/lua/custom/mappings.lua Add .config/nvim/lua/custom/plugins.lua Add .config/nvim/lua/plugins/configs/cmp.lua Add .config/nvim/lua/plugins/configs/lazy_nvim.lua Add .config/nvim/lua/plugins/configs/lspconfig.lua Add .config/nvim/lua/plugins/configs/mason.lua Add .config/nvim/lua/plugins/configs/nvimtree.lua Add .config/nvim/lua/plugins/configs/others.lua Add .config/nvim/lua/plugins/configs/telescope.lua Add .config/nvim/lua/plugins/configs/treesitter.lua

---
+++ dot_config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,18 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "gruvchad",
+  theme_toggle = { "gruvchad", "gruvbox_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+
+return M
---
+++ dot_config/nvim/lua/custom/configs/lspconfig.lua
@@ -0,0 +1,47 @@
+--- This file load the lsp-plugin and allow you to override / setup.
+--- Plugin is defined in plugins.lua with {"lsp", opt={}}
+
+local default_on_attach = require("plugins.configs.lspconfig").on_attach
+local default_capabilities = require("plugins.configs.lspconfig").capabilities
+
+--- lspconfig is lsp_plugin.opt.servers options
+--- setup is lspconfig.opt.setup options
+local lspconfig = require "lspconfig"
+local util = require "lspconfig/util"
+
+local on_attach_with_formating = function(client, bufnr)
+  default_on_attach(client, bufnr)
+  client.server_capabilities.documentFormattingProvider = true
+  client.resolved_capabilities.document_formatting = true
+  client.resolved_capabilities.document_range_formatting = true
+end
+
+lspconfig.pyright.setup {
+  on_attach = default_on_attach,
+  capabilities = default_capabilities,
+  filetypes = { "python" },
+}
+
+lspconfig.rust_analyzer.setup {
+  on_attach = on_attach_with_formating,
+  capabilities = default_capabilities,
+  filetypes = { "rust" },
+  root_dir = util.root_pattern "Cargo.toml",
+  settings = {
+    ["rust-analyzer"] = {
+      cargo = {
+        allFeatures = true,
+        loadOutDirsFromCheck = true,
+        runBuildScripts = true,
+      },
+      procMacro = {
+        enable = true,
+        ignored = {
+          ["async-trait"] = { "async_trait" },
+          ["napi-derive"] = { "napi" },
+          ["async-recursion"] = { "async_recursion" },
+        },
+      },
+    },
+  },
+}
