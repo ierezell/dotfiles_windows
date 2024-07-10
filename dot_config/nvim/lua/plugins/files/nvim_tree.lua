2ab4bbf7e7d4f96b0d0d910c45a3af1974de9695
Pierre Snell <psnell@biotwin.ai>
2024-07-10 13:42:34 -0400

    Update .config/nvim/lua/plugins/files/nvim_tree.lua Update .config/nvim/lua/settings/files/nvim_tree.lua

--- dot_config/nvim/lua/plugins/files/nvim_tree.lua
+++ dot_config/nvim/lua/plugins/files/nvim_tree.lua
@@ -6,11 +6,11 @@ return {
     'nvim-lua/plenary.nvim',
     "antosha417/nvim-lsp-file-operations",
     "nvim-tree/nvim-web-devicons",
-    -- 'b0o/nvim-tree-preview.lua',
+  --  'b0o/nvim-tree-preview.lua',
   },
   config = function()
     require("nvim-tree").setup {}
     require("lsp-file-operations").setup()
-    -- require("nvim-tree-preview").setup()
+--    require("nvim-tree-preview").setup()
   end,
 }
