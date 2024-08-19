6c5eac7e6be1d2caade33151f400d8464d73d316
Pierre Snell <psnell@biotwin.ai>
2024-08-19 15:18:59 -0400

    Update .config/nvim/lua/settings/keymaps.lua

--- dot_config/nvim/lua/settings/keymaps.lua
+++ dot_config/nvim/lua/settings/keymaps.lua
@@ -1,19 +1,30 @@
 -- register which-key VISUAL mode
 -- required for visual <leader>hs (hunk stage) to work
-require('which-key').register({ ['<leader>'] = { name = 'VISUAL <leader>' } }, { mode = 'v' })
+require('which-key').register({ "<leader>", group = "VISUAL <leader>", mode = "v" })
 
 require('which-key').register {
-    ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },      --
-    ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },  --
-    ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },       --
-    ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },    --
-    ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },    --
-    ['<leader>f'] = { name = '[F]loat', _ = 'which_key_ignore' },     --
-    ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },    --
-    ['<leader>h'] = { name = '[H]arpoon', _ = 'which_key_ignore' },   --
-    ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' }, --
-    ["["] = { name = 'Previous', _ = 'which_key_ignore' },            --
-    ["]"] = { name = 'Next', _ = 'which_key_ignore' }                 --
+    { "<leader>c", group = "[C]ode" },
+    { "<leader>c_", hidden = true },
+    { "<leader>d", group = "[D]ocument" },
+    { "<leader>d_", hidden = true },
+    { "<leader>f", group = "[F]loat" },
+    { "<leader>f_", hidden = true },
+    { "<leader>g", group = "[G]it" },
+    { "<leader>g_", hidden = true },
+    { "<leader>h", group = "[H]arpoon" },
+    { "<leader>h_", hidden = true },
+    { "<leader>r", group = "[R]ename" },
+    { "<leader>r_", hidden = true },
+    { "<leader>s", group = "[S]earch" },
+    { "<leader>s_", hidden = true },
+    { "<leader>t", group = "[T]oggle" },
+    { "<leader>t_", hidden = true },
+    { "<leader>w", group = "[W]orkspace" },
+    { "<leader>w_", hidden = true },
+    { "[", group = "Previous" },
+    { "[_", hidden = true },
+    { "]", group = "Next" },
+    { "]_", hidden = true },
 }
 
 vim.api.nvim_create_user_command('LiveGrepGitRoot', telescope_live_grep_git_root, {})
